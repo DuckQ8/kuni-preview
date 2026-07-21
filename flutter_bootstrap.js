19b0c1371dc277a4b997ebdb7f10cebe85aac520
Raising Canes <Imfzextra@gmail.com>
2026-07-21 08:50:23 +0300

    Deploy: auto professor enrichment (AI triage + review dashboard) + agentic assistant with confirmation cards

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1062237324" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3511944265" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
